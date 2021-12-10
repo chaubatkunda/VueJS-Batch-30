959eaaecd54cd7de50c0c66d8c50e281c3d22f18
chaubatkunda <maniklusi31@gmail.com>
2021-12-10 16:16:17 +0700

    tugas 5 seelsai

---
+++ Tugas-5-Javascript-Array-Function-Object/index.js
@@ -0,0 +1,21 @@
+// ? Soal 1
+var daftarHewan = ["2. Komodo", "5. Buaya", "3. Cicak", "4. Ular", "1. Tokek"];
+daftarHewan.sort();
+console.log(daftarHewan);
+console.log("==============================================")
+// ? Soal 2
+function introduce() {
+    var data = { nama: "Chau Batkunda", umur: 26, alamat: "Jl Joyosary", hobby: "Ngoding" }
+    console.log(data);
+}
+introduce()
+
+console.log("==============================================")
+
+//? Soal 3
+function hitung_huruf_vokal(hitung_1, hitung_2) {
+    var hitung_1 = ("Muhammad")
+    var hitung_2 = ("Iqbal")
+    console.log(hitung_1.length , hitung_2.length) // 3 2
+}
+hitung_huruf_vokal();
